e9404b369252f44ff3e7b627fc90f57f875d1551
Nagy Dávid <95david.nagy@gmail.com>
2019-03-14 11:35:57 +0100

    Basic files

---
+++ src/test/java/com/nezopont/NezopontApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nezopont;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NezopontApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
