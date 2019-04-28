5cbc2e7debd44863ed96c06c5ef155c5f17f7f23
hckr05 <43717685+hckr05@users.noreply.github.com>
2019-04-28 21:04:16 +0200

    Fixxing.

---
+++ runET.bat
@@ -0,0 +1,5 @@
+mvn sonar:sonar \
+  -Dsonar.projectKey=Almafa1 \
+  -Dsonar.organization=hckr05-github \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=41b915442a2efa40f6e9a4e28dd0fe357f232a9c
